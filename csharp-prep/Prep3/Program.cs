58891e62e9b93a1e250c2451e05809557e3f294e
MOCAHE <can22002@byui.edu>
2023-09-05 17:46:20 -0600

    Segunda parte completada

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,5 +6,58 @@ class Program
     {
         Console.WriteLine("Hello Prep3 World!");
         Console.WriteLine();
+
+        // INICIO PRIMERA PARTE
+        // Console.WriteLine("Whats is the magic number?: ");
+        // string mNumber = Console.ReadLine();
+        // int magicNumber = int.Parse(mNumber);
+
+        // Console.WriteLine("What is your guess?: ");
+        // string gNumber = Console.ReadLine();
+        // int guessNumber = int.Parse(gNumber);
+
+        // if (guessNumber > magicNumber)
+        // {
+        //     Console.WriteLine("Lower");
+        // }
+        // else if (guessNumber < magicNumber)
+        // {
+        //     Console.WriteLine("Higher");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("You guessed it!");
+        // }
+        // Console.WriteLine();
+        // FIN PRIMERA PARTE
+
+        // INICIO SEGUNDA PARTE
+        Console.Write("Whats is the magic number?: ");
+        string mNumber = Console.ReadLine();
+        int magicNumber = int.Parse(mNumber);
+
+        int guessNumber = -1;
+
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess?: ");
+            string gNumber = Console.ReadLine();
+            guessNumber = int.Parse(gNumber);
+
+            if (guessNumber > magicNumber)
+            {
+                Console.Write("Lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.Write("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            Console.WriteLine();
+        }
+        // FIN SEGUNDA PARTE
     }
 }
