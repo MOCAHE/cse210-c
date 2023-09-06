203527ed436238871daa0334f8290b4584c0d8fb
MOCAHE <can22002@byui.edu>
2023-09-05 18:21:36 -0600

    Fin de tercera parte y 1 stretch challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,59 +5,92 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
-        Console.WriteLine();
+
 
         // INICIO PRIMERA PARTE
-        // Console.WriteLine("Whats is the magic number?: ");
+        // Console.Write("Whats is the magic number?: ");
         // string mNumber = Console.ReadLine();
         // int magicNumber = int.Parse(mNumber);
 
-        // Console.WriteLine("What is your guess?: ");
+        // Console.Write("What is your guess?: ");
         // string gNumber = Console.ReadLine();
         // int guessNumber = int.Parse(gNumber);
 
         // if (guessNumber > magicNumber)
         // {
-        //     Console.WriteLine("Lower");
+        //     Console.Write("Lower");
         // }
         // else if (guessNumber < magicNumber)
         // {
-        //     Console.WriteLine("Higher");
+        //     Console.Write("Higher");
         // }
         // else
         // {
-        //     Console.WriteLine("You guessed it!");
+        //     Console.Write("You guessed it!");
         // }
-        // Console.WriteLine();
+        // Console.Write();
         // FIN PRIMERA PARTE
 
-        // INICIO SEGUNDA PARTE
-        Console.Write("Whats is the magic number?: ");
-        string mNumber = Console.ReadLine();
-        int magicNumber = int.Parse(mNumber);
 
+        // INICIO SEGUNDA PARTE
+        // Console.Write("Whats is the magic number?: ");
+        // string mNumber = Console.ReadLine();
+        // int magicNumber = int.Parse(mNumber);
+
+        // int guessNumber = -1;
+
+        // while (guessNumber != magicNumber)
+        // {
+        //     Console.Write("What is your guess?: ");
+        //     string gNumber = Console.ReadLine();
+        //     guessNumber = int.Parse(gNumber);
+
+        //     if (guessNumber > magicNumber)
+        //     {
+        //         Console.Write("Lower");
+        //     }
+        //     else if (guessNumber < magicNumber)
+        //     {
+        //         Console.Write("Higher");
+        //     }
+        //     else
+        //     {
+        //         Console.Write("You guessed it!");
+        //     }
+        //     Console.WriteLine();
+        // }
+        // FIN SEGUNDA PARTE
+
+
+        // INICIO TERCERA PARTE & STRETCH CHALLENGE
+        Random randomGenerator = new();
+        int number = randomGenerator.Next(1, 100);
+        string keepPlaying = "yes";
         int guessNumber = -1;
 
-        while (guessNumber != magicNumber)
+        while (keepPlaying == "yes")
         {
             Console.Write("What is your guess?: ");
             string gNumber = Console.ReadLine();
             guessNumber = int.Parse(gNumber);
 
-            if (guessNumber > magicNumber)
+            if (guessNumber > number)
             {
                 Console.Write("Lower");
             }
-            else if (guessNumber < magicNumber)
+            else if (guessNumber < number)
             {
                 Console.Write("Higher");
             }
             else
             {
                 Console.WriteLine("You guessed it!");
+                Console.WriteLine();
+                Console.Write("Do you want to keep playing? (yes/no): ");
+                keepPlaying = Console.ReadLine();
             }
             Console.WriteLine();
         }
-        // FIN SEGUNDA PARTE
+        // FIN TERCERA PARTE & STRETCH CHALLENGE
     }
 }
